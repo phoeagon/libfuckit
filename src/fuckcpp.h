074a7453004f24d2641d1ff2a5b39104b726bd98
phoeagon <phoeagon@gmail.com>
2015-04-12 03:36:13 +0800

    init

---
+++ src/fuckcpp.h
@@ -0,0 +1,20 @@
+#ifndef FUCKCPP_H
+#define FUCKCPP_H
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <udis86.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <ucontext.h>
+#include <execinfo.h>
+
+
+struct fuckcpp_t {
+    void* _default_entry;
+    ud_t * ud_obj;
+}_fuckcpp;
+
+int _fuckcpp_init();
+#endif
---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include "fuckcpp.h"
+int main()
+{
+    _fuckcpp_init();
+    *((int*)123) = 144;
+    printf("recovered\n");
+    *((int*)34) = 144;
+    printf("recovered\n");
+    *((int*)43) = 144;
+    printf("recovered\n");
+    __asm__("push $123\n"
+    "ret");
+    printf("recovered\n");
+    int a = 4 / 0;
+    printf("recovered\n");
+    return 0;
+}
